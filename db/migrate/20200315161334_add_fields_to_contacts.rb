e18c8882a1585df56106557c7c130d0474e66a6c
RusuAlexPaul <rp.rusupaul@gmail.com>
2020-03-15 18:36:12 +0200

    Another step

---
+++ db/migrate/20200315161334_add_fields_to_contacts.rb
@@ -0,0 +1,9 @@
+class AddFieldsToContacts < ActiveRecord::Migration[6.0]
+  def change
+    add_column :contacts, :phone, :integer
+    add_column :contacts, :country, :text
+    add_column :contacts, :city, :text
+    add_column :contacts, :gender, :text
+    add_column :contacts, :age, :integer
+  end
+end
