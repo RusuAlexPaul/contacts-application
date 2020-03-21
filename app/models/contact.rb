08752b105e3b1272c74649bd93b5d74560992969
RusuAlexPaul <rp.rusupaul@gmail.com>
2020-03-22 00:43:19 +0200

    Change the phone maximum number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ApplicationRecord
   validates :email, confirmation: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  validates :phone,:presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 15 }
+  validates :phone,:presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 10 }
 end
