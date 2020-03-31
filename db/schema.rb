a8faa1374bb04cefc818e9885a3b474d7b6511a5
RusuAlexPaul <rp.rusupaul@gmail.com>
2020-03-31 23:17:48 +0300

    new updates

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_163538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
