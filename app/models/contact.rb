2b74b7bbcc366b3f8e7d609809c7f03ff3dd9ecf
RusuAlexPaul <rp.rusupaul@gmail.com>
2020-05-26 13:05:41 +0300

    New updates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
   validates :email, confirmation: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  validates :phone,:presence => true, :numericality => true
+  validates :phone,:presence => true, :numericality => true, :length => { :minimum => 10, :maximum => 10 }
   validates :gender, presence: true
 end
